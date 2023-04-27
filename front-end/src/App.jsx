0d6338d3e972c8b17dd77b9c837db8828d8f3ba0
KairosMP <109192850+KairosMP@users.noreply.github.com>
2023-04-26 21:25:08 -0400

    teste de api

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -10,19 +10,43 @@ import {
   Button,
 } from '@chakra-ui/react';
 
+import  {useState}  from 'react';
+
 import axios from 'axios';
 
 function App() {
-  const formSubmitCallback = (form) => {
-    axios.post('http://localhost:3000', form)
+  const [form, setForm] = useState({
+    nome: '',
+    email: '',
+    cpf: '',
+    fone: '',
+    cep: '',
+    log: '',
+    bairro: '',
+    cidade: '',
+    estado: '',
+    numero: '',
+  });
+
+  const formSubmitCallback = () => {
+    event.preventDefault();
+    axios
+      .post('http://localhost:3000/Forms', form)
       .then((response) => {
         console.log(response);
       })
       .catch((error) => {
         console.error(error);
-    });
-  }
+      });
+  };
 
+  const handleInputChange = (event) => {
+    const { id, value } = event.target;
+    setForm((prevForm) => ({
+      ...prevForm,
+      [id]: value,
+    }));
+  };
 
   return (
     <Box h="100vh">
@@ -48,111 +72,105 @@ function App() {
           p="6"
           boxShadow="0 1px 2px #ccc"
         >
-          <FormControl display="flex" flexDir="column" gap="4">
-            <HStack spacing="4">
-              <Box w="100%">
-                <FormLabel htmlFor="nome">Nome Completo</FormLabel>
-                <Input id="nome" />
-              </Box>
-              <Box w="100%">
-                <FormLabel htmlFor="email">E-mail</FormLabel>
-                <Input id="email" type="email" />
-              </Box>
-            </HStack>
-            <HStack spacing="4">
-              <Box w="100%">
-                <FormLabel htmlFor="cpf">CPF</FormLabel>
-                <Input id="cpf" />
-              </Box>
-
-              <Box w="100%">
-                <FormLabel htmlFor="fone">Telefone</FormLabel>
-                <Input id="fone" type="tel" />
-              </Box>
-            </HStack>
-
-            <HStack spacing="4">
-              <Box w="100%">
-                <FormLabel htmlFor="cep">Cep</FormLabel>
-                <Input id="cep" type="number" />
-              </Box>
-
-              <Box w="100%">
-                <FormLabel htmlFor="log">Logradouro</FormLabel>
-                <Input id="log" />
-              </Box>
-            </HStack>
-
-            <HStack spacing="4">
-              <Box w="100%">
-                <FormLabel htmlFor="bairro">Bairro</FormLabel>
-                <Input id="bairro" />
-              </Box>
-
-              <Box w="100%">
-                <FormLabel htmlFor="cidade">Cidade</FormLabel>
-                <Input id="cidade" />
-              </Box>
-            </HStack>
-
-            <HStack spacing="4">
-              <Box w="100%">
-                <FormLabel>Estados</FormLabel>
-                <Select placeholder="Escolha um estado">
-                  <option value="AC">Acre</option>
-                  <option value="AL">Alagoas</option>
-                  <option value="AP">Amapá</option>
-                  <option value="AM">Amazonas</option>
-                  <option value="BA">Bahia</option>
-                  <option value="CE">Ceará</option>
-                  <option value="DF">Distrito Federal</option>
-                  <option value="ES">Espírito Santo</option>
-                  <option value="GO">Goiás</option>
-                  <option value="MA">Maranhão</option>
-                  <option value="MT">Mato Grosso</option>
-                  <option value="MS">Mato Grosso do Sul</option>
-                  <option value="MG">Minas Gerais</option>
-                  <option value="PA">Pará</option>
-                  <option value="PB">Paraíba</option>
-                  <option value="PR">Paraná</option>
-                  <option value="PE">Pernambuco</option>
-                  <option value="PI">Piauí</option>
-                  <option value="RJ">Rio de Janeiro</option>
-                  <option value="RN">Rio Grande do Norte</option>
-                  <option value="RS">Rio Grande do Sul</option>
-                  <option value="RO">Rondônia</option>
-                  <option value="RR">Roraima</option>
-                  <option value="SC">Santa Catarina</option>
-                  <option value="SP">São Paulo</option>
-                  <option value="SE">Sergipe</option>
-                  <option value="TO">Tocantins</option>
-                </Select>
-              </Box>
-
-              <Box w="20%">
-                <FormLabel htmlFor="numero">numero</FormLabel>
-                <Input id="numero" />
-              </Box>
-            </HStack>
-
-            <HStack justify="center">
-              <Button
-                w={240}
-                p="6"
-                type="submit"
-                bg="#283CA1"
-                color="white"
-                fontWeight="bold"
-                fontSize="xl"
-                mt="2"
-                _hover={{ bg: '#2B3D6D' }}
-              >
-                Enviar
-              </Button>
-
-              {formSubmitCallback()}
-            </HStack>
-          </FormControl>
+          <form style={{
+            width: '100vw'
+          }} onSubmit={formSubmitCallback}>
+            <FormControl display="flex" flexDir="column" gap="4">
+              <HStack spacing="4">
+                <Box w="100%">
+                  <FormLabel htmlFor="nome">Nome Completo</FormLabel>
+                  <Input id="nome" onChange={handleInputChange} />
+                </Box>
+                <Box w="100%">
+                  <FormLabel htmlFor="email">E-mail</FormLabel>
+                  <Input id="email" type="email" onChange={handleInputChange} />
+                </Box>
+              </HStack>
+              <HStack spacing="4">
+                <Box w="100%">
+                  <FormLabel htmlFor="cpf">CPF</FormLabel>
+                  <Input id="cpf" onChange={handleInputChange} />
+                </Box>
+                <Box w="100%">
+                  <FormLabel htmlFor="fone">Telefone</FormLabel>
+                  <Input id="fone" type="tel" onChange={handleInputChange} />
+                </Box>
+              </HStack>
+              <HStack spacing="4">
+                <Box w="100%">
+                  <FormLabel htmlFor="cep">Cep</FormLabel>
+                  <Input id="cep" type="number" onChange={handleInputChange} />
+                </Box>
+                <Box w="100%">
+                  <FormLabel htmlFor="log">Logradouro</FormLabel>
+                  <Input id="log" onChange={handleInputChange} />
+                </Box>
+              </HStack>
+              <HStack spacing="4">
+                <Box w="100%">
+                  <FormLabel htmlFor="bairro">Bairro</FormLabel>
+                  <Input id="bairro" onChange={handleInputChange} />
+                </Box>
+                <Box w="100%">
+                  <FormLabel htmlFor="cidade">Cidade</FormLabel>
+                  <Input id="cidade" onChange={handleInputChange} />
+                </Box>
+              </HStack>
+              <HStack spacing="4">
+                <Box w="100%">
+                  <FormLabel>Estados</FormLabel>
+                  <Select placeholder="Escolha um estado">
+                    <option value="AC">Acre</option>
+                    <option value="AL">Alagoas</option>
+                    <option value="AP">Amapá</option>
+                    <option value="AM">Amazonas</option>
+                    <option value="BA">Bahia</option>
+                    <option value="CE">Ceará</option>
+                    <option value="DF">Distrito Federal</option>
+                    <option value="ES">Espírito Santo</option>
+                    <option value="GO">Goiás</option>
+                    <option value="MA">Maranhão</option>
+                    <option value="MT">Mato Grosso</option>
+                    <option value="MS">Mato Grosso do Sul</option>
+                    <option value="MG">Minas Gerais</option>
+                    <option value="PA">Pará</option>
+                    <option value="PB">Paraíba</option>
+                    <option value="PR">Paraná</option>
+                    <option value="PE">Pernambuco</option>
+                    <option value="PI">Piauí</option>
+                    <option value="RJ">Rio de Janeiro</option>
+                    <option value="RN">Rio Grande do Norte</option>
+                    <option value="RS">Rio Grande do Sul</option>
+                    <option value="RO">Rondônia</option>
+                    <option value="RR">Roraima</option>
+                    <option value="SC">Santa Catarina</option>
+                    <option value="SP">São Paulo</option>
+                    <option value="SE">Sergipe</option>
+                    <option value="TO">Tocantins</option>
+                  </Select>
+                </Box>
+                <Box w="20%">
+                  <FormLabel htmlFor="numero">numero</FormLabel>
+                  <Input id="numero" onChange={handleInputChange}/>
+                </Box>
+              </HStack>
+              <HStack justify="center">
+                <Button
+                  w={240}
+                  p="6"
+                  type="submit"
+                  bg="#283CA1"
+                  color="white"
+                  fontWeight="bold"
+                  fontSize="xl"
+                  mt="2"
+                  _hover={{ bg: '#2B3D6D' }}
+                >
+                  Enviar
+                </Button>
+              </HStack>
+            </FormControl>
+          </form>
         </Center>
         <Flex
           as="footer"
@@ -163,12 +181,8 @@ function App() {
           justify="center"
           bg=" #283CA1 "
         >
-          <Center
-            color={'whiteAlpha.900'}
-            w={'120'}
-            fontSize={'2xl'}
-          >
-             Developer by Miller &copy; 2023
+          <Center color={'whiteAlpha.900'} w={'120'} fontSize={'2xl'}>
+            Developer by Miller &copy; 2023
           </Center>
         </Flex>
       </Flex>
